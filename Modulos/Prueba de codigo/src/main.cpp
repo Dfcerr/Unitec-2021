c433d63e0f67ef057ae354d394898f4eedeb8d1e
dfcerr <rayotinnn@hotmail.com>
2021-05-21 11:14:58 -0300

    agregado de orden

--- Modulos/Prueba de codigo/src/main.cpp
+++ Modulos/Prueba de codigo/src/main.cpp
@@ -8,5 +8,4 @@ void loop() {
   Serial.print('Hola Mundo, esta es una prueba de GitHub');
   Serial.print('Esta es una segunda linea!!!!!!!!!');
   Serial.print('Chau Mundo');
-  serial.print('cuchau');
 }
