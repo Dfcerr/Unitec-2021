87f09cb3aac42b0e5063a1661d9e91e986823ce9
dfcerr <rayotinnn@hotmail.com>
2021-10-21 05:23:37 -0300

    color, distancia y sonido acabaods

--- Modulos/Funciones/Sensor de Color/Gy9960/src/Final.cpp
+++ Modulos/Funciones/Sensor de Color/Gy9960/src/Final.cpp
@@ -1,8 +1,8 @@
-#include <SparkFun_APDS9960.h>
+#include <SparkFun_APDS9960.h> 
 #include <Wire.h>
 
 #define led 4
-#define boton 5
+#define botonColor 5
 #define buzzer 6
 
 SparkFun_APDS9960 apds = SparkFun_APDS9960();
@@ -10,83 +10,84 @@ SparkFun_APDS9960 apds = SparkFun_APDS9960();
 void setup() 
 {
   Serial.begin(9600);
-//iniciacion de la libreria para el GY9960
-//**********************************************************************
+  pinMode(botonColor, INPUT_PULLUP);
 //color
   apds.init();
   apds.enableLightSensor(false);
 //proximidad
   apds.enableProximitySensor(false);
-  apds.setProximityGain(PGAIN_4X);
-//**********************************************************************
+  apds.setProximityGain(PGAIN_1X);
 }
 
 void loop() 
 {
     delay(400); 
-    
-    if(digitalRead(boton))
-    {
-        crgbc();    
-        Serial.println("*************************************");
-    }
+    if(!digitalRead(botonColor))
+        determinarColor();    
 }
 
 //***********************************************************************************************************
-void crgbc()// en esta funcion se tiene que intentar determinar un color en concreto
+void determinarColor()// en esta funcion se tiene que intentar determinar un color en concreto
 {
-//------------------------------------------------------------
   digitalWrite(led, HIGH);// prende un led blanco para mejorar la lectura
-//------------------------------------------------------------
+  delay(200);//para que bi se active el proceso de salir de la funcion
 // variables declaradas 
-//------------------------------------------------------------
   int cont = 0;
   uint8_t proximidad;
   uint16_t R;
   uint16_t G;
   uint16_t B;
-  float Ambiental;
-//------------------------------------------------------------
+  uint16_t A;
+  float ambiental;
 //se fija que este a una distancia optima  
-//----------------------------------------------------------------------------------
   while(cont<40)//espera 2s
   {
     apds.readProximity(proximidad);// lee la distancia del 9960
-    if(proximidad>170&&proximidad<255)// si esta esta en estos parametros empieza a contar por 2s
-    {
-      cont++;// suma 1us al contador
-      analogWrite(buzzer, 0);// avisa que esta en pocision correcta
+    analogWrite(buzzer, 0);//apaga el indicador para que vibre pausadamente
+    
+    if(!digitalRead(botonColor)){//si vuelve a presionar el boton sale de la funcion
+      digitalWrite(led, LOW);// prende un led blanco para mejorar la lectura
+      return;
     }
-    else// si se sale del rango resetea el contador
-    {
+    
+    if(proximidad >= 255)// si esta esta en estos parametros empieza a contar por 2s
+      cont++;
+
+    else{// si se sale del rango resetea el contador
       cont = 0;// reset del contador de us
-      analogWrite(buzzer, 1);// indica que no esta en posicioon
+      analogWrite(buzzer, 200);// indica que no esta en posicion
     }
     delay(50);
   }
   analogWrite(buzzer, 0);// si todo salio bien apaga el indicador
 //------------------------------------------------------------
 // saca los colores puros
-//------------------------------------------------------------
-  apds.readAmbientLight(Ambiental);;//  saca el valor ambiental
+  apds.readAmbientLight(A);//  saca el valor ambiental
   apds.readRedLight(R); 
   apds.readGreenLight(G); 
   apds.readBlueLight(B); 
 //------------------------------------------------------------
 //  saca el valor ambiental
-//----------------------------------------------------------------------------------  
-//  convierte este valor de 0 a 255
-  R = map((((R/Ambiental)*2.8)*100), 0, 100, 0, 255);
-  G = map((((G/Ambiental)*2.8)*100), 0, 100, 0, 255);
-  B = map((((B/Ambiental)*2.8)*100), 0, 100, 0, 255);
+  ambiental = A;
+  //map((((R/ambiental))*300), 0, 100, 0, 255)
+  R = map((((R/ambiental))*300), 0, 110, 0, 255);//  convierte este valor de 0 a 255
+  G = map((((G/ambiental))*300), 0, 125, 0, 255);
+  B = map((((B/ambiental))*300), 0, 105, 0, 255);;
   digitalWrite(led, LOW);// apaga el led blanco que ya no es necesario
-//----------------------------------------------------------------------------------
-//  en este espacio con if segun un parametros se veria que colores van a ser  
-
-//-----------------------------------------------------------------------------------
 // Envia el color(en este caso como aun no hice el if pongo el rgb mejorado)
+
+  if (R >= G && R >= B)
+    Serial.println("rojo");
+  if (G >= R && G >= B)
+    Serial.println("verde");
+  if (B >= G && B >= R)
+    Serial.println("azul");
+
+  Serial.println(ambiental);
   Serial.println(R);
   Serial.println(G);
   Serial.println(B);
+
 }
+
 //***********************************************************************************************************
--- Modulos/Funciones/Sensor de Distancia/Hcsr04/src/prueba1.cpp
+++ Modulos/Funciones/Sensor de Distancia/Hcsr04/src/prueba1.cpp
@@ -1,52 +1,47 @@
 // Hola :D
 #define TRIG 10
 #define ECO  9
-#define boton_d  8
+#define botonDistancia  5
 #define buzzer 6
 
 int distancia;
-int dl = 0;
-int mh = 1;
+bool modoAhorro = 0;
+
+unsigned long axuMillis;
+
 void setup() 
 {
-	pinMode(TRIG, OUTPUT);
+    pinMode(TRIG, OUTPUT);
+    pinMode(botonDistancia, INPUT_PULLUP);
+    axuMillis = millis();
     Serial.begin(9600);
 } 
 void loop()
 {   
-    delay(200);dl += 200;
+    s_ultra();
+    delay(200); 
 }
 //***********************************************************************************
 int m_distancia()
 {
-    dl = 0;//resetea el tiempo del contador
-//------------------------------------------------------------
     int duracion, aux = 0;
-    byte error = 0;
-//------------------------------------------------------------
-//  saca un promedio en un tiempo de 50ms    
-//-----------------------------------------------------   
+    uint8_t error = 0;
+//  saca un promedio en un tiempo de 100ms
     for (byte veces = 0; veces < 10; veces++)
     {
-//-----------------------------------------------------        
 //      mide la distancia
-//----------------------------------------------------- 
         digitalWrite(TRIG, HIGH);
         delay(1);
         digitalWrite(TRIG, LOW);
         duracion = pulseIn(ECO,HIGH);
-//----------------------------------------------------- 
 //      se fija de que no haya errores
-//----------------------------------------------------- 
         if(duracion > 0)
             aux += duracion/58.2;
         else
             error++;
-        delay(5);
+        delay(100);
     }
-//----------------------------------------------------- 
 //  segun los erroes que haya da un resultado
-//----------------------------------------------------- 
     if(error < 3)
     {
         aux = aux/(10 - error);
@@ -54,22 +49,22 @@ int m_distancia()
     }
     else
         return 999;
-//------------------------------------------------------------
 }
 //***********************************************************************************
 void s_ultra()
 {
-    if(digitalRead(boton_d))
+    if(!digitalRead(botonDistancia))
     {
         distancia = m_distancia();
         Serial.println(distancia);
     }
-    if(dl > 1000 && !mh)
+    if(!modoAhorro && (millis() - axuMillis) > 1000)//si a pasado 1s toma una medida
     {
+        axuMillis = millis();
         distancia = m_distancia();
         if(distancia < 70)
         {
-            analogWrite(buzzer, 5);
+            analogWrite(buzzer, 200);
             Serial.println("cuidado");
             delay(500);
             analogWrite(buzzer, 0);
